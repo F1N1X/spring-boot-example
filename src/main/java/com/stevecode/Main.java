c3be05142fd663bf5d9d963eb73ce5685f8e8512
F1NiX <s.heiza@ymail.com>
2024-09-16 20:43:36 +0200

    Record the Short-Way
    Records short way for this implementation...

--- src/main/java/com/stevecode/Main.java
+++ src/main/java/com/stevecode/Main.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-
 @RestController
 public class Main {
     public static void main(String[] args) {
@@ -19,5 +18,24 @@ public class Main {
         return new GreedResponse("Whats up");
     }
 
-    record GreedResponse(String greet) {}
+    //record GreedResponse(String greet) {}
+
+    class GreetResponse {
+        private final String greet;
+
+        GreetResponse(String greet) {
+            this.greet = greet;
+        }
+
+        public String getGreet() {
+            return greet;
+        }
+
+        @Override
+        public String toString() {
+            return greet;
+        }
+
+        //..hashCode-Method
+    }
 }
