8308b33d97fec29f4d572ad578b611c15e69f494
F1NiX <s.heiza@ymail.com>
2024-10-02 16:30:07 +0200

    Create Model

--- src/main/java/com/stevecode/Main.java
+++ src/main/java/com/stevecode/Main.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
 @SpringBootApplication
 @RestController
@@ -16,25 +17,76 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/greet")
-    public GreedResponse greet(@RequestParam(value = "name", required = false) String name) {
-        //http://localhost:8080/greet?name=Peter
-        String greetMessage = name == null || name.isBlank() ? "Hello" : "Hello " + name;
-        GreedResponse response = new GreedResponse(
-                greetMessage,
-                List.of("Python","Java","JavaScript"),
-                new Person("Martin", 30, 10_000)
-                );
+    //alt+einf
+    class Customer {
+        private int id;
+        private String name;
+        private String email;
+        private int age;
 
-        return response;
+        public Customer() {}
+
+        public Customer(int id, String name, String email, int age) {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+            this.age = age;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Customer customer = (Customer) o;
+            return id == customer.id && age == customer.age && Objects.equals(name, customer.name) && Objects.equals(email, customer.email);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name, email, age);
+        }
+
+        @Override
+        public String toString() {
+            return "Customer{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", email='" + email + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
     }
-
-    record Person(String name, int age, double savings){}
-
-    record GreedResponse(
-            String greet,
-            List<String> favoriteProgrammingLanguages,
-            Person person
-            ) {}
 
 }
