a87992ab1b2a53f7239d64e3988e136c204dcc1a
F1NiX <s.heiza@ymail.com>
2024-10-05 23:44:09 +0200

    Update CustomerDao.java

--- src/main/java/com/stevecode/customer/CustomerDao.java
+++ src/main/java/com/stevecode/customer/CustomerDao.java
@@ -3,6 +3,8 @@ package com.stevecode.customer;
 import java.util.List;
 import java.util.Optional;
 
+
+//Reimplement with RL-Project
 public interface CustomerDao {
     List<Customer> selectAllCustomers();
     Optional<Customer> selectCustomerById(Integer id);
