5da22721420937aa058357db3ae31e9044a9e36f
F1NiX <s.heiza@ymail.com>
2024-10-14 20:20:01 +0200

    Big Serial showing

--- src/main/resources/db/migration/V1__Initial_Setup.sql
+++ src/main/resources/db/migration/V1__Initial_Setup.sql
@@ -2,7 +2,7 @@ CREATE SEQUENCE customer_id_sequence;
 
 
 CREATE TABLE customer(
-    id BIGSERIAL PRIMARY KEY ,
+    id BIGINT DEFAULT nextval('customer_id_sequence') PRIMARY KEY ,
     name TEXT NOT NULL ,
     email TEXT NOT NULL ,
     age INT NOT NULL
