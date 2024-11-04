3c138477b490d21394b9c73002fadcac19f4cdcf
F1NiX <s.heiza@ymail.com>
2024-11-04 21:45:08 +0100

    Update CustomerRepositoryTest.java

--- src/test/java/com/stevecode/customer/CustomerRepositoryTest.java
+++ src/test/java/com/stevecode/customer/CustomerRepositoryTest.java
@@ -1,21 +1,17 @@
 package com.stevecode.customer;
 
+import com.stevecode.AbstractTestcontainers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.cassandra.DataCassandraTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-//Load ALL BEANS from Context
 @DataJpaTest
-//Connect to Docker-Compose
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class CustomerRepositoryTest {
+class CustomerRepositoryTest extends AbstractTestcontainers {
 
     //DI like constructor-call
     @Autowired
