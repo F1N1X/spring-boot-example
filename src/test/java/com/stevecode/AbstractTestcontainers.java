e24a8c770f1c2443e80f695ed033b2b09fee13fe
F1NiX <s.heiza@ymail.com>
2024-11-05 17:08:38 +0100

    Update AbstractTestcontainers.java

--- src/test/java/com/stevecode/AbstractTestcontainers.java
+++ src/test/java/com/stevecode/AbstractTestcontainers.java
@@ -1,5 +1,6 @@
 package com.stevecode;
 
+import com.github.javafaker.Faker;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.jdbc.DataSourceBuilder;
@@ -12,54 +13,58 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import javax.sql.DataSource;
 
-
 @Testcontainers
 public abstract class AbstractTestcontainers {
+
     @BeforeAll
     static void beforeAll() {
-        Flyway flyway = Flyway.configure().dataSource(
-                postgreSQLContainer.getJdbcUrl(),
-                postgreSQLContainer.getUsername(),
-                postgreSQLContainer.getPassword()
-        ).load();
+        Flyway flyway = Flyway
+                .configure()
+                .dataSource(
+                        postgreSQLContainer.getJdbcUrl(),
+                        postgreSQLContainer.getUsername(),
+                        postgreSQLContainer.getPassword()
+                ).load();
         flyway.migrate();
-        System.out.println();
     }
 
     @Container
-    protected static final PostgreSQLContainer<?> postgreSQLContainer
-            = new PostgreSQLContainer<>("postgres:16")
-            .withDatabaseName("stevecode-dao-unit-test")
-            .withUsername("stevecode")
-            .withPassword("password");
+    protected static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:latest")
+                    .withDatabaseName("amigoscode-dao-unit-test")
+                    .withUsername("amigoscode")
+                    .withPassword("password");
 
-
-    //Its the Source-Path -> application.yml
     @DynamicPropertySource
-    private static void registerDataSourceProperties(DynamicPropertyRegistry registry) {
+    private static void registerDataSourceProperties(
+            DynamicPropertyRegistry registry) {
         registry.add(
                 "spring.datasource.url",
-                postgreSQLContainer::getJdbcUrl);
+                postgreSQLContainer::getJdbcUrl
+        );
         registry.add(
                 "spring.datasource.username",
-                postgreSQLContainer::getUsername);
+                postgreSQLContainer::getUsername
+        );
         registry.add(
-                "spring.datasource.url",
-                postgreSQLContainer::getPassword);
+                "spring.datasource.password",
+                postgreSQLContainer::getPassword
+        );
     }
 
     private static DataSource getDataSource() {
-        return
-                DataSourceBuilder.create()
-                        .driverClassName(postgreSQLContainer.getDriverClassName())
-                        .url(postgreSQLContainer.getJdbcUrl())
-                        .username(postgreSQLContainer.getUsername())
-                        .password(postgreSQLContainer.getPassword())
-                        .build();
+        return DataSourceBuilder.create()
+                .driverClassName(postgreSQLContainer.getDriverClassName())
+                .url(postgreSQLContainer.getJdbcUrl())
+                .username(postgreSQLContainer.getUsername())
+                .password(postgreSQLContainer.getPassword())
+                .build();
     }
 
     protected static JdbcTemplate getJdbcTemplate() {
         return new JdbcTemplate(getDataSource());
     }
+
+    protected static final Faker FAKER = new Faker();
 
 }
