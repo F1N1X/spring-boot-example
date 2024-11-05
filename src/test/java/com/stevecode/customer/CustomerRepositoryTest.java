e24409e45200988a9013a8ad50defcaa465280b9
F1NiX <s.heiza@ymail.com>
2024-11-05 16:19:42 +0100

    Start with not all Beans
    90 Beans .. mutch better than 230

--- src/test/java/com/stevecode/customer/CustomerRepositoryTest.java
+++ src/test/java/com/stevecode/customer/CustomerRepositoryTest.java
@@ -3,10 +3,12 @@ package com.stevecode.customer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CustomerRepositoryTest {
 
     @Autowired
