38bcf2ec6adec2f4ceeace703a9cc90b832c0cbc
F1NiX <s.heiza@ymail.com>
2024-11-05 16:08:27 +0100

    SpringBootTest Setup
    Load mutch Beans.. Long Load

---
+++ src/test/java/com/stevecode/customer/CustomerRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.stevecode.customer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+class CustomerRepositoryTest {
+
+    @Autowired
+    private CustomerRepository underTest;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        System.out.println(applicationContext.getBeanDefinitionCount());
+    }
+
+    @Test
+    void existsCustomerByEmail() {
+    }
+
+    @Test
+    void existsCustomerById() {
+    }
+}
