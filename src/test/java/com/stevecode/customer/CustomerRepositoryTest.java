1c89ee882315f85cde195911a6de7b527922ea40
F1NiX <s.heiza@ymail.com>
2024-11-05 16:01:59 +0100

    Update CustomerRepositoryTest.java

--- src/test/java/com/stevecode/customer/CustomerRepositoryTest.java
+++ src/test/java/com/stevecode/customer/CustomerRepositoryTest.java
@@ -6,11 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
-
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest
 class CustomerRepositoryTest extends AbstractTestcontainers {
 
     //DI like constructor-call
